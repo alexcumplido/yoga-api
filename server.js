0b80b96a181ea11068cbd41ef4995550b5a9d65e
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-01-29 11:11:22 +0100

    Refactored get method for each endpoint

--- server.js
+++ server.js
@@ -15,30 +15,35 @@ app.get("/", (request, response) => {
     response.json(baseURL);
   });
 
-app.get(
-  "/api/yoga/routeCategories/:categories/:categoryName?",
-  (request, response) => {
-    const categories = request.params.categories;
-    const category = request.params.categoryName;
-    if (categories && category === undefined) {
-      response.json(yogacategories);
-    } else if (categories && category) {
-      const singleCategory = yogacategories.items.find(function (element) {
-        return element.name.toLowerCase() === category.toLowerCase();
-      });
-      response.json(singleCategory);
-    }
+app.get("/api/yoga/:categories/", (request, response) => {
+  const categories = request.params.categories;
+  if (categories) {
+    response.json(yogacategories);
   }
-);
+});
 
-app.get("/api/yoga/routePoses/:poses/:poseName?", (request, response) => {
+app.get("/api/yoga/category/:categoryName/", (request, response) => {
+  const category = request.params.categoryName;
+  if (category) {
+    const singleCategory = yogacategories.items.find(function (element) {
+      return element.name.toLowerCase() === category.toLowerCase();
+    });
+    response.json(singleCategory);
+  }
+});
+
+app.get("/api/yoga/:poses/", (request, response) => {
   const poses = request.params.poses;
-  const posture = request.params.poseName;
-  if (poses && posture === undefined) {
+  if (poses) {
     response.json(yogaposes);
-  } else if (poses == "poses" && posture) {
+  }
+});
+
+app.get("/api/yoga/pose/:poseName/", (request, response) => {
+  const pose = request.params.poseName;
+  if (pose) {
     const singlePose = yogaposes.items.find(function (element) {
-      return element.english_name.toLowerCase() === posture.toLowerCase();
+      return element.english_name.toLowerCase() === pose.toLowerCase();
     });
     response.json(singlePose);
   }
