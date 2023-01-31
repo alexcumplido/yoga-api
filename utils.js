80f99f08bfaacbe5230091f2207ea046d407007c
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-01-31 21:56:51 +0100

    Implemented mongodb poses and categories endpoints

---
+++ utils.js
@@ -0,0 +1,6 @@
+// categories.items.forEach(function (element) {
+//   console.log(element);
+//   db.collection("categories")
+//     .insertOne({ element })
+//     .catch((error) => console.error(error));
+// });
