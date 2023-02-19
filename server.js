affa7f56661f910337133433535a384f6a4b8900
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-02-19 17:38:47 +0100

    Loaded data to SQLite

--- server.js
+++ server.js
@@ -5,12 +5,33 @@ const cors = require("cors");
 const routerApi = require("./routes/index");
 const { logErrors, errorHandler } = require("./middlewares/errorHandler");
 const PORT = process.env.PORT;
+const Database = require("better-sqlite3");
+const db = new Database("./db/database.db", { verbose: console.log });
 
 app.use(cors());
 app.use(logger("dev"));
 
 app.get("/", async (req, res) => {
   res.status(200).sendFile(__dirname + "/index.html");
+});
+
+const transitive = require("./resources/transitiveSQL.json");
+app.get("/posestransitive", async (req, res) => {
+  const queryInsert = db.prepare(
+    "INSERT INTO poses_transitive (pose_id, category_id, difficulty_id) VALUES (?, ?, ?)"
+  );
+
+  transitive.poses_category.forEach(function (element) {
+    queryInsert.run(
+      element.pose_id,
+      element.category_id,
+      element.difficulty_id
+    );
+  });
+
+  const queryGet = db.prepare("SELECT * FROM  poses_transitive");
+  const transitivePoses = queryGet.all();
+  res.json(transitivePoses);
 });
 
 routerApi(app);
@@ -21,30 +42,3 @@ app.use(errorHandler);
 app.listen(PORT || 8000, () => {
   console.log(`Server running in port number ${PORT || 8000}`);
 });
-
-// const Database = require("better-sqlite3");
-// const db = new Database("./db/database.db", { verbose: console.log });
-// const posesJson = require("./db/poses.json");
-// app.get("/database", async (req, res) => {
-//   const queryInsert = db.prepare(
-//     "INSERT INTO poses (sanskrit_name, english_name, img_url) VALUES (?, ?, ?)"
-//   );
-
-//   posesJson.items.forEach(function (element) {
-//     queryInsert.run(
-//       element.pose_id,
-//       element.english_name,
-//       element.sanskrit_name,
-//       element.difficulty
-//       element.description,
-//       element.benefits,
-//       element.img_url_svg
-//       element.img_url_jpg,
-//       element.img_url_svg_alt,
-//     );
-//   });
-
-//   const queryGet = db.prepare("SELECT * FROM  poses");
-//   const poses = queryGet.all();
-//   res.json(poses);
-// });
