7d315a6fdd900d532063700675e4395d3cede629
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-02-25 18:40:17 +0100

    Removed non used code

--- server.js
+++ server.js
@@ -5,33 +5,12 @@ const cors = require("cors");
 const routerApi = require("./routes/index");
 const { logErrors, errorHandler } = require("./middlewares/errorHandler");
 const PORT = process.env.PORT;
-const Database = require("better-sqlite3");
-const db = new Database("./db/database.db", { verbose: console.log });
 
 app.use(cors());
 app.use(logger("dev"));
 
 app.get("/", async (req, res) => {
   res.status(200).sendFile(__dirname + "/index.html");
-});
-
-const transitive = require("./resources/transitiveSQL.json");
-app.get("/posestransitive", async (req, res) => {
-  const queryInsert = db.prepare(
-    "INSERT INTO poses_transitive (pose_id, category_id, difficulty_id) VALUES (?, ?, ?)"
-  );
-
-  transitive.poses_category.forEach(function (element) {
-    queryInsert.run(
-      element.pose_id,
-      element.category_id,
-      element.difficulty_id
-    );
-  });
-
-  const queryGet = db.prepare("SELECT * FROM  poses_transitive");
-  const transitivePoses = queryGet.all();
-  res.json(transitivePoses);
 });
 
 routerApi(app);
