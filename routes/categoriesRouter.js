42566ed20af94f8dc36d27419e83853f9ba0d130
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-02-25 18:40:49 +0100

    Implemented new endpoints

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 const services = require("../services/services");
 const validatorHandler = require("../middlewares/validatorHandler");
-const { schemaName } = require("../schemas/schemas");
+const { schemaName, schemaId } = require("../schemas/schemas");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -15,7 +15,25 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get(
-  "/:name",
+  "/:id",
+  validatorHandler(schemaId, "params"),
+  async (req, res, next) => {
+    const id = req.params.id;
+    try {
+      const data = await services.getCategoryById(id);
+      if (data) {
+        res.status(200).json(data).end();
+      } else {
+        res.status(404).json({ message: "category not found" }).end();
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get(
+  "/catName/:name",
   validatorHandler(schemaName, "params"),
   async (req, res, next) => {
     const name = req.params.name;
