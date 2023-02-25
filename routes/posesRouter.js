97a15ac689b2bbf1f4f10abc375438752472ce1e
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-02-25 18:40:57 +0100

    Implemented new endpoints

--- routes/posesRouter.js
+++ routes/posesRouter.js
@@ -7,10 +7,16 @@ const { schemaName, schemaId } = require("../schemas/schemas");
 
 router.get("/", async (req, res, next) => {
   const sort = req.query.sort === "true" ? true : false;
+  const level = req.query.level;
+  const category = req.query.category;
   try {
     let data;
     if (sort) {
       data = await services.getPosesSorted();
+    } else if (category) {
+      data = await services.getPosesByCategoryAndDifficulty(category, level);
+    } else if (level) {
+      data = await services.getPosesByDifficulty(level);
     } else {
       data = await services.getPoses();
     }
@@ -21,12 +27,12 @@ router.get("/", async (req, res, next) => {
 });
 
 router.get(
-  "/poseName/:name",
-  validatorHandler(schemaName, "params"),
+  "/:id",
+  validatorHandler(schemaId, "params"),
   async (req, res, next) => {
-    const name = req.params.name;
+    const poseId = req.params.id;
     try {
-      const data = await services.getPoseByName(name);
+      const data = await services.getPoseById(poseId);
       if (data) {
         res.status(200).json(data).end();
       } else {
@@ -39,12 +45,12 @@ router.get(
 );
 
 router.get(
-  "/poseId/:id",
-  validatorHandler(schemaId, "params"),
+  "/poseName/:name",
+  validatorHandler(schemaName, "params"),
   async (req, res, next) => {
-    const poseId = req.params.id;
+    const name = req.params.name;
     try {
-      const data = await services.getPoseById(poseId);
+      const data = await services.getPoseByName(name);
       if (data) {
         res.status(200).json(data).end();
       } else {
