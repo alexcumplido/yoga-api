e7c7f81e08d521e3c54d9eb2fbc135d4d452c0ee
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-03-02 11:59:44 +0100

    Reduced to only one endpoint with query params

--- routes/posesRouter.js
+++ routes/posesRouter.js
@@ -2,64 +2,22 @@ const { json } = require("express");
 const express = require("express");
 const services = require("../services/services");
 const router = express.Router();
-const validatorHandler = require("../middlewares/validatorHandler");
-const { schemaName, schemaId } = require("../schemas/schemas");
 
 router.get("/", async (req, res, next) => {
-  const sort = req.query.sort === "true" ? true : false;
-  const level = req.query.level;
-  const category = req.query.category;
   try {
     let data;
-    if (sort) {
-      data = await services.getPosesSorted();
-    } else if (category) {
-      data = await services.getPosesByCategoryAndDifficulty(category, level);
-    } else if (level) {
-      data = await services.getPosesByDifficulty(level);
-    } else {
+    if (Object.keys(req.query).length === 0) {
       data = await services.getPoses();
+    } else {
+      data = await services.getPosesByParams(req.query);
+      if (!data) {
+        res.status(404).json({ message: "pose not found" }).end();
+      }
     }
     res.status(200).json(data).end();
   } catch (error) {
     next(error);
   }
 });
-
-router.get(
-  "/:id",
-  validatorHandler(schemaId, "params"),
-  async (req, res, next) => {
-    const poseId = req.params.id;
-    try {
-      const data = await services.getPoseById(poseId);
-      if (data) {
-        res.status(200).json(data).end();
-      } else {
-        res.status(404).json({ message: "pose not found" }).end();
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-);
-
-router.get(
-  "/name/:name",
-  validatorHandler(schemaName, "params"),
-  async (req, res, next) => {
-    const name = req.params.name;
-    try {
-      const data = await services.getPoseByName(name);
-      if (data) {
-        res.status(200).json(data).end();
-      } else {
-        res.status(404).json({ message: "pose not found" }).end();
-      }
-    } catch (error) {
-      next(error);
-    }
-  }
-);
 
 module.exports = router;
