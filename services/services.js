339c0680aa7016e344279f3ccf0a306727d65590
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-03-02 11:59:59 +0100

    Implemented poses filtering by query params

--- services/services.js
+++ services/services.js
@@ -26,93 +26,35 @@ async function getCategories() {
   return categories;
 }
 
-async function getCategoryById(id) {
-  const queryCategory = dbLite.prepare(
-    `SELECT * 
+const rootQuery = `SELECT * 
     FROM categories
-    WHERE categories.id = ?`
-  );
-  const rowsCategory = queryCategory.get(id);
-  const queryPoses = dbLite.prepare(
-    `SELECT 
+    WHERE`;
+
+const baseQuery = `SELECT 
     poses.id, category_name, english_name, sanskrit_name_adapted, sanskrit_name, translation_name, pose_description, pose_benefits, 
     url_svg, url_png, url_svg_alt 
     FROM poses 
       INNER JOIN transitive_poses ON poses.id = transitive_poses.pose_id 
-      INNER JOIN categories ON categories.id = transitive_poses.category_id 
-      WHERE categories.id = ?`
-  );
+      INNER JOIN categories ON categories.id = transitive_poses.category_id`;
+
+async function getCategoryById(id) {
+  const queryCategory = dbLite.prepare(`${rootQuery} categories.id = ?`);
+  const rowsCategory = queryCategory.get(id);
+  const queryPoses = dbLite.prepare(`${baseQuery} WHERE categories.id = ?`);
   const rowsPoses = queryPoses.all(rowsCategory.id);
   return { ...rowsCategory, poses: [...rowsPoses] };
 }
 
 async function getCategoryByName(name) {
   const queryCategory = dbLite.prepare(
-    `SELECT *
-    FROM categories
-    WHERE categories.category_name = ? COLLATE NOCASE`
+    `${rootQuery} categories.category_name = ? COLLATE NOCASE`
   );
   const rowsCategory = queryCategory.get(name);
   const queryPoses = dbLite.prepare(
-    `SELECT
-    poses.id, category_name, english_name, sanskrit_name_adapted, sanskrit_name, translation_name, pose_description, pose_benefits,
-    url_svg, url_png, url_svg_alt
-    FROM poses
-      INNER JOIN transitive_poses ON poses.id = transitive_poses.pose_id
-      INNER JOIN categories ON categories.id = transitive_poses.category_id
-      WHERE categories.category_name = ?`
+    `${baseQuery} WHERE categories.category_name = ?`
   );
   const rowsPoses = queryPoses.all(rowsCategory.category_name);
   return { ...rowsCategory, poses: [...rowsPoses] };
-}
-
-async function getPoses() {
-  const query = dbLite.prepare("SELECT * FROM poses");
-  const rows = query.all();
-  return rows;
-}
-
-async function getPosesSorted() {
-  const query = dbLite.prepare(
-    "SELECT * FROM poses ORDER BY poses.english_name ASC"
-  );
-  const rows = query.all();
-  return rows;
-}
-
-async function getPoseByName(name) {
-  const query = dbLite.prepare(
-    "SELECT * FROM poses WHERE poses.english_name = ? COLLATE NOCASE"
-  );
-  const row = query.get(name);
-  return row;
-}
-
-async function getPoseById(id) {
-  const query = dbLite.prepare("SELECT * FROM poses WHERE id = ?");
-  const row = query.get(id);
-  return row;
-}
-
-async function getPosesByDifficulty(level) {
-  const queryDifficulty = dbLite.prepare(
-    `SELECT * 
-    FROM difficulty
-    WHERE difficulty.difficulty_level = ? COLLATE NOCASE`
-  );
-  const rowsDifficulty = queryDifficulty.get(level);
-  const queryPoses = dbLite.prepare(
-    `SELECT DISTINCT
-    poses.id, difficulty_level, english_name, sanskrit_name_adapted, sanskrit_name, translation_name, pose_description, pose_benefits, 
-    url_svg, url_png, url_svg_alt
-    FROM poses
-	    INNER JOIN transitive_poses ON poses.id = transitive_poses.pose_id
-	    INNER JOIN difficulty ON difficulty.id = transitive_poses.difficulty_id
-	    WHERE difficulty.difficulty_level = ? COLLATE NOCASE 
-  `
-  );
-  const rowsPoses = queryPoses.all(rowsDifficulty.difficulty_level);
-  return { ...rowsDifficulty, poses: [...rowsPoses] };
 }
 
 async function getPosesByCategoryAndDifficulty(category, difficulty) {
@@ -138,15 +80,57 @@ async function getPosesByCategoryAndDifficulty(category, difficulty) {
   return { ...rowsCategory, poses: [...rows] };
 }
 
+async function getPoses() {
+  const query = dbLite.prepare(
+    "SELECT * FROM poses ORDER BY poses.english_name ASC"
+  );
+  const rows = query.all();
+  return rows;
+}
+
+async function getPosesByParams(params) {
+  const { id, name, level } = params;
+  let rows;
+  if (level) {
+    rows = await getPosesByLevel(level);
+  } else if (id || name) {
+    const queryId = id && `id = ${id}`;
+    const queryName = name && `poses.english_name = '${name}' COLLATE NOCASE`;
+    const query = dbLite.prepare(
+      `SELECT * FROM poses WHERE ${queryId || queryName}`
+    );
+    rows = query.get();
+  }
+  return rows;
+}
+
+async function getPosesByLevel(level) {
+  const queryDifficulty = dbLite.prepare(
+    `SELECT * 
+    FROM difficulty 
+    WHERE difficulty.difficulty_level = ? COLLATE NOCASE`
+  );
+  const rowsDifficulty = queryDifficulty.get(level);
+  const queryPoses = dbLite.prepare(
+    `SELECT DISTINCT
+    poses.id, difficulty_level, english_name, sanskrit_name_adapted, sanskrit_name, translation_name, pose_description, pose_benefits, 
+    url_svg, url_png, url_svg_alt
+    FROM poses
+	    INNER JOIN transitive_poses ON poses.id = transitive_poses.pose_id
+	    INNER JOIN difficulty ON difficulty.id = transitive_poses.difficulty_id
+	    WHERE difficulty.difficulty_level = ? COLLATE NOCASE 
+  `
+  );
+  const rowsPoses = queryPoses.all(rowsDifficulty.difficulty_level);
+  return { ...rowsDifficulty, poses: [...rowsPoses] };
+}
+
 module.exports = {
   getBaseURL,
   getCategories,
   getCategoryByName,
   getPoses,
-  getPosesSorted,
+  getPosesByParams,
   getCategoryById,
-  getPoseByName,
-  getPoseById,
-  getPosesByDifficulty,
   getPosesByCategoryAndDifficulty,
 };
