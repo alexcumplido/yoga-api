8802ca09e048729073eba14ce989dd9bdfba7a13
Alexandre Cumplido Bou <acumpbo@gmail.com>
2023-03-10 11:40:42 +0100

    Included new schemas

--- schemas/schemas.js
+++ schemas/schemas.js
@@ -1,16 +1,22 @@
 const Joi = require("joi");
 const id = Joi.string().regex(/^\d+$/).min(1).max(2);
-const name = Joi.string().min(3).max(50);
+const paramString = Joi.string().min(3).max(50);
 
-const schemaName = Joi.object({
-  name: name.required(),
-});
+// const schemaName = Joi.object({
+//   name: name.required(),
+// });
 
 const schemaId = Joi.object({
   id: id.required(),
 });
 
+const schemaParamsPoses = Joi.object({
+  id: id.required(),
+  name: paramString.required(),
+  level: paramString.required(),
+});
+
 module.exports = {
   schemaId,
-  schemaName,
+  schemaParamsPoses,
 };
